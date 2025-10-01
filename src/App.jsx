5669e0fdd59c4e86eaa32b512019b97cf5b2351b
Luis Medina Coca <luismedinacoca@gmail.com>
2025-10-01 06:34:01 -0300

    Section 5: Working With Components, Props and JSX Lecture 035: Components as Building Blocks Lecture 036: Creating and Reusing a Component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Pizza from "./components/pizza";
+function App() {
+  const x = "Luiggie";
+  return (
+    <>
+      <h1>Hello React!!!</h1>
+      <p>Hello here from this paragraph</p>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  );
+}
+
+export default App;
