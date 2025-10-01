f47e2cd6b7ac97f468586733d114d8058b9e28be
Luis Medina Coca <luismedinacoca@gmail.com>
2025-10-01 07:01:59 -0300

    Section 5: Working With Components, Props and JSX Lecture 039: JavaScript Logic in Components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  //else alert("Sorry, we're closed!");
+
+  console.log(hour);
+  console.log(isOpen);
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
