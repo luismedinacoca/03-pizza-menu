4cc149d987ed69346b88d0d9cdd7765559650ea4
Luis Medina Coca <luismedinacoca@gmail.com>
2025-10-03 05:59:52 -0300

    Section 5: Working with Components, Props and JSX Lecture 042:  Passing and Receiving Props

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,9 +4,24 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Margherita"
+        ingredients="Tomato and mozarella"
+        photoName="pizzas/margherita.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        price={12}
+        photoName="pizzas/spinaci.jpg"
+      />
     </main>
   );
 };
--- src/components/pizza.jsx
+++ src/components/pizza.jsx
@@ -45,12 +45,16 @@ const pizzaData = [
   },
 ];
 
-const Pizza = () => {
+const Pizza = (props) => {
+  console.log(props);
   return (
-    <div>
-      <img src="pizzas/salamino.jpg" alt="Pizza Salamino" />
-      <h3>Pizza Salamino</h3>
-      <p>Tomato, mozarella, and pepperoni</p>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
     </div>
   );
 };
